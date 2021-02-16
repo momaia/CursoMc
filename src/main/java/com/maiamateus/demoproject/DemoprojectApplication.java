a1cd6f17a81b29d2644dede02215a411a57b2b32
Mateus Otavio <maiamateusotavio@gmail.com>
2021-02-16 17:59:09 -0300

    Criando operacao de instanciacao

--- src/main/java/com/maiamateus/demoproject/DemoprojectApplication.java
+++ src/main/java/com/maiamateus/demoproject/DemoprojectApplication.java
@@ -1,13 +1,32 @@
 package com.maiamateus.demoproject;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DemoprojectApplication {
+import com.maiamateus.demoproject.domain.Categoria;
+import com.maiamateus.demoproject.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class DemoprojectApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository; 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoprojectApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informáica");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
