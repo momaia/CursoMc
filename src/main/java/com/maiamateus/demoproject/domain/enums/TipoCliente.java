5958fafd2ff0a2f9b9f49b59c09614ff0ce1925f
Mateus Otavio <maiamateusotavio@gmail.com>
2021-02-17 21:10:35 -0300

    Repositorios Cidade e Endereco

---
+++ src/main/java/com/maiamateus/demoproject/domain/enums/TipoCliente.java
@@ -0,0 +1,38 @@
+package com.maiamateus.demoproject.domain.enums;
+
+public enum TipoCliente {
+
+	PESSOAFISICA(0, "Pessoa física"),
+	PESSOAJURIDICA(1, "Pessoa jurídica");
+	
+	private Integer cod;
+	private String descricao;
+	
+	private TipoCliente(Integer cod, String descricao) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+
+	public Integer getCod() {
+		return cod;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public static TipoCliente toEnum(Integer cod) {
+
+		if (cod == null) {
+			return null;
+		}
+
+		for (TipoCliente x : TipoCliente.values()) {
+			if (cod.equals(x.getCod())) {
+				return x;
+			}
+		}
+
+		throw new IllegalArgumentException("Id inválido: " + cod);
+	}
+}
