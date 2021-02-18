219a0874bdc6742ee788bf400d270a15cf49abaa
Mateus Otavio <maiamateusotavio@gmail.com>
2021-02-17 21:56:57 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/maiamateus/demoproject/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.maiamateus.demoproject.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.maiamateus.demoproject.domain.Cliente;
+import com.maiamateus.demoproject.repositories.ClienteRepository;
+import com.maiamateus.demoproject.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findById(id).orElse(null);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+					+ ", Tipo: " + Cliente.class.getName());
+		}
+		return obj;
+	}
+}
